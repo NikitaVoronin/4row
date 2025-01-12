8a5dd5e0094bf550b976fd7c019ff5ae66bc6f71
Иван Ухов <ivanuchov07@gmail.com>
2025-01-12 23:01:04 +0300

    Добавлены анимации падения ящиков !!!исправить фпс

--- Board logic.py
+++ Board logic.py
@@ -1,4 +1,5 @@
 import pygame
+import time
 from LoadTextures import *
 from Boxes import *
 
@@ -22,12 +23,13 @@ class Board:
     def render(self, screen):
         for i in range(self.height):
             for j in range(self.width):
-                if self.board[i][j] == 1:
-                    Box_O(self.left + self.cell_size * j, self.top + self.cell_size * i, all_sprites)
-                elif self.board[i][j] == 2:
-                    Box_X(self.left + self.cell_size * j, self.top + self.cell_size * i, all_sprites)
-
                 cords = (self.left + self.cell_size * j, self.top + self.cell_size * i, self.cell_size, self.cell_size)
+                if self.board[i][j] != 0 and len(falling_boxes.sprites()) == 0:
+                    if self.board[i][j] == 1:
+                        BoxO(cords[0], cords[1], placed_boxes)
+                    elif self.board[i][j] == 2:
+                        BoxX(cords[0], cords[1], placed_boxes)
+
                 pygame.draw.rect(screen, (255, 255, 255), cords, 1)
 
     def get_cell(self, mouse_pos):
@@ -39,12 +41,22 @@ class Board:
             return None
 
     def on_click(self, cell_coords):
+        if cell_coords is None or cell_coords[0] > 6 or cell_coords[1] > 5:
+            return
         x, y = cell_coords
+
+        if self.board[y][x] == 0:
+            if self.player == 1:
+                BoxO(self.left + self.cell_size * x, self.top + self.cell_size * y, falling_boxes)
+            elif self.player == 2:
+                BoxX(self.left + self.cell_size * x, self.top + self.cell_size * y, falling_boxes)
+
         while self.board[y][x] == 0:
             if y == 5 or self.board[y + 1][x] != 0:
                 break
             else:
                 y += 1
+
         if self.board[y][x] == 0:
             self.board[y][x] = self.player
             self.player = 1 if self.player == 2 else 2
@@ -54,26 +66,48 @@ class Board:
         self.on_click(cell)
 
 
-screen = pygame.display.set_mode((1920, 1080))
+screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 board = Board(7, 6)
-board.set_view(487, 135, 135)
+board.set_view(487, 180, 135)
 
-all_sprites = pygame.sprite.Group()
-back = pygame.sprite.Sprite(all_sprites)
-back.image = load_image("back.jpg")
+placed_boxes = pygame.sprite.Group()
+background = pygame.sprite.Group()
+ground_border = pygame.sprite.Group()
+falling_boxes = pygame.sprite.Group()
+
+back = pygame.sprite.Sprite(background)
+back.image = load_image("Background.jpg")
 back.rect = back.image.get_rect()
-
 back.rect.x = 0
 back.rect.y = 0
 
+
+ground = pygame.sprite.Sprite(ground_border)
+ground.rect = pygame.Rect(board.left, board.top + board.cell_size * board.height,
+                          board.left + board.cell_size * board.width, board.top + board.cell_size * board.height)
+
+prev_time = time.perf_counter()
+current_time = prev_time
+FPS = 60
+STEP_TIME = 1./FPS
 running = True
+
 while running:
+    prev_time = current_time
+    current_time = time.perf_counter()
+    dt = current_time - prev_time
+
+    while time.perf_counter() < (current_time + STEP_TIME):
+        pass
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             board.get_click(event.pos)
-    screen.fill((0, 0, 0))
-    all_sprites.draw(screen)
+    background.draw(screen)
+    falling_boxes.draw(screen)
+    falling_boxes.update(None, ground_border, placed_boxes)
+    placed_boxes.draw(screen)
     board.render(screen)
-    pygame.display.flip()
+    pygame.display.update()
--- Boxes.py
+++ Boxes.py
@@ -2,25 +2,53 @@ import pygame
 from LoadTextures import *
 
 
-class Box_O(pygame.sprite.Sprite):
-    image = load_image('o.png')
+class BoxO(pygame.sprite.Sprite):
+    image = load_image('O_Box.png')
+    image_selected = load_image('O_AllocatedBox.png')
     image = pygame.transform.scale(image, (135, 135))
+    image_selected = pygame.transform.scale(image_selected, (135, 135))
 
     def __init__(self, x, y, *group):
         super().__init__(*group)
-        self.image = Box_O.image
+        self.image = BoxO.image
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
+        self.velocity = 25
+
+    def update(self, *args):
+        self.rect = self.rect.move(0, self.velocity)
+        if pygame.sprite.spritecollideany(self, args[1]) or pygame.sprite.spritecollideany(self, args[2]):
+            self.velocity = 0
+            self.kill()
+
+        if args[0] is not None:
+            if (args and args[0].type == pygame.MOUSEBUTTONDOWN and
+                    self.rect.collidepoint(args[0].pos)):
+                self.image = self.image_selected
 
 
-class Box_X(pygame.sprite.Sprite):
-    image = load_image('x.png')
+class BoxX(pygame.sprite.Sprite):
+    image = load_image('X_Box.png')
+    image_selected = load_image('X_AllocatedBox.png')
     image = pygame.transform.scale(image, (135, 135))
+    image_selected = pygame.transform.scale(image_selected, (135, 135))
 
     def __init__(self, x, y, *group):
         super().__init__(*group)
-        self.image = Box_X.image
+        self.image = BoxX.image
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
+        self.velocity = 25
+
+    def update(self, *args):
+        self.rect = self.rect.move(0, self.velocity)
+        if pygame.sprite.spritecollideany(self, args[1]) or pygame.sprite.spritecollideany(self, args[2]):
+            self.velocity = 0
+            self.kill()
+
+        if args[0] is not None:
+            if (args and args[0].type == pygame.MOUSEBUTTONDOWN and
+                    self.rect.collidepoint(args[0].pos)):
+                self.image = self.image_selected
